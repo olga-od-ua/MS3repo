8ec5b689ca1f473f73b474a4ca918062bdf15297
olga-od-ua <olgasokolova21@gmail.com>
2021-09-17 10:13:46 +0000

    Add delete recipe from db functionality

--- app.py
+++ app.py
@@ -155,6 +155,13 @@ def edit_recipe(recipe_id):
         cooking_time=cooking_time)
 
 
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Recipe Deleted")
+    return redirect(url_for("get_recipes"))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
